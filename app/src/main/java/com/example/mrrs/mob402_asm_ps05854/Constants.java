d3505abe5b80ff330471fd3b4a5f824bc70def3c
Tam Nguyen <tam.nguyen.dev@pplus.vn>
2018-08-14 16:16:07 +0700

    Fixing Login Register

--- app/src/main/java/com/example/mrrs/mob402_asm_ps05854/Constants.java
+++ app/src/main/java/com/example/mrrs/mob402_asm_ps05854/Constants.java
@@ -1,7 +1,7 @@
 package com.example.mrrs.mob402_asm_ps05854;
 
 public class Constants {
-    public static final String BASE_URL = "http://192.168.254.2/";
+    public static final String BASE_URL = "http://192.168.1.6/";
     public static final String REGISTER_OPERATION = "register";
     public static final String LOGIN_OPERATION = "login";
     public static final String CHANGE_PASSWORD_OPERATION = "chgPass";
@@ -18,15 +18,15 @@ public class Constants {
     public static final String TAG = "LearnAPI";
 
     public static String url_all_products =
-            "http://192.168.254.2/learn-login-register/get_all_product.php";
+            "http://192.168.1.6/learn-login-register/get_all_product.php";
     public static String url_create_products =
-            "http://192.168.254.2/learn-login-register/create_product.php";
+            "http://192.168.1.6/learn-login-register/create_product.php";
     public static String url_product_detail =
-            "http://192.168.254.2/learn-login-register/get_product_detail.php";
+            "http://192.168.1.6/learn-login-register/get_product_detail.php";
     public static String url_update_product =
-            "http://192.168.254.2/learn-login-register/update_product.php";
+            "http://192.168.1.6/learn-login-register/update_product.php";
     public static String url_delete_product =
-            "http://192.168.254.2/learn-login-register/delete_product.php";
+            "http://192.168.1.6 /learn-login-register/delete_product.php";
     // JSON Node names
     public static final String TAG_SUCCESS = "success";
     public static final String TAG_PRODUCTS = "products";
