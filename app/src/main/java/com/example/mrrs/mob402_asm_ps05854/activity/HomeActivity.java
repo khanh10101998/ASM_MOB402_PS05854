587c33347c3a9b731e2b3077675df4c3127b4456
khanh10101998 <khanh10101998@gmail.com>
2018-08-18 17:40:55 +0700

    get data

--- app/src/main/java/com/example/mrrs/mob402_asm_ps05854/activity/HomeActivity.java
+++ app/src/main/java/com/example/mrrs/mob402_asm_ps05854/activity/HomeActivity.java
@@ -18,8 +18,9 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     LoadAllProductsTask loadAllProductsTask;
     CreateNewProductTask newProductTask;
     ViewFlipper ViewFlipper_home;
-    ImageView img1_home,img2_home,img3_home;
-    Animation in,out;
+    ImageView img1_home, img2_home, img3_home, iv_mercedes_g65, iv_mercedes_E300, iv_mercedes_S500, iv_mercedes_gt3;
+    Animation in, out;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,18 +48,26 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         LoadImage("https://images.hgmsites.net/hug/2017-volvo-s90_100579091_h.jpg", img1_home);
         LoadImage("https://di-uploads-pod6.s3.amazonaws.com/landroverwestchester/uploads/2016/08/2017-Land-Rover-Evoque-SE.jpg", img2_home);
         LoadImage("https://lc-cdn.sixt.io/Images/Cars/BMW/USA_BMW_7er_L/Content_Box/half-660x330/A0197012.jpg", img3_home);
+
+        LoadImage("http://image.trucktrend.com/f/48796851+w+h+q80+re0+cr1/2013-mercedes-benz-g65-amg-front.jpg", iv_mercedes_g65);
+        LoadImage("https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/dsc_4666.jpg?itok=Niukj0Hq", iv_mercedes_E300);
+        LoadImage("https://www.cars.co.za/carimages_gen/Mercedes-Benz-S-Class/Mercedes-Benz-S-Class-S500-coupe-AMG-Line_MercS-Cc5c6_2l.jpg", iv_mercedes_S500);
+        LoadImage("http://blog-int.kwautomotive.net/wp-content/uploads/2016/09/gallery_17.jpg", iv_mercedes_gt3);
+
+
     }
 
     private void LoadImage(String url, ImageView imageView) {
         Picasso.with(this).load(url)
+                .resize(600,300)
                 .placeholder(R.drawable.ic_place_holder)
                 .error(R.drawable.ic_error)
                 .into(imageView);
     }
 
     private void displayViewFipper() {
-        in = AnimationUtils.loadAnimation(this,R.anim.fade_in);
-        out = AnimationUtils.loadAnimation(this,R.anim.fade_out);
+        in = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        out = AnimationUtils.loadAnimation(this, R.anim.fade_out);
         ViewFlipper_home.setInAnimation(in);
         ViewFlipper_home.setOutAnimation(out);
         ViewFlipper_home.setFlipInterval(3000);
@@ -71,33 +80,49 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         img1_home = findViewById(R.id.img1_home);
         img2_home = findViewById(R.id.img2_home);
         img3_home = findViewById(R.id.img3_home);
+
+        iv_mercedes_g65 = findViewById(R.id.iv_mercedes_g65);
+        iv_mercedes_E300 = findViewById(R.id.iv_mercedes_E300);
+        iv_mercedes_S500 = findViewById(R.id.iv_mercedes_S500);
+        iv_mercedes_gt3 = findViewById(R.id.iv_mercedes_gt3);
     }
 
 
-
-    public void INTENT(Class c){
+    public void INTENT(Class c) {
         Intent intent = new Intent(HomeActivity.this, c);
         startActivity(intent);
     }
 
     private void AddToCart(String userId, String name, String price, String description, String urlImage) {
         newProductTask = new CreateNewProductTask(this);
-        newProductTask.execute(userId,name,price, description, urlImage);
+        newProductTask.execute(userId, name, price, description, urlImage);
     }
 
     @Override
     public void onClick(View v) {
         ViewFlipper_home.startFlipping();
         ViewFlipper_home.setFlipInterval(3000);
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.img1_home:
-
                 INTENT(CartActivity.class);
                 break;
             case R.id.img2_home:
                 INTENT(CartActivity.class);
                 break;
             case R.id.img3_home:
+                INTENT(CartActivity.class);
+                break;
+            case R.id.iv_mercedes_g65:
+                INTENT(CartActivity.class);
+                break;
+            case R.id.iv_mercedes_E300:
+                INTENT(CartActivity.class);
+                break;
+            case R.id.iv_mercedes_S500:
+                INTENT(CartActivity.class);
+                break;
+
+            case R.id.iv_mercedes_gt3:
                 INTENT(CartActivity.class);
                 break;
         }
